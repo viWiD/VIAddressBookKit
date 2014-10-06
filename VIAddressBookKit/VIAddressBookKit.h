1e378b3f5cdf4dc4e11219f7c09eb0fb3ef44de2
Nils Fischer <n.fischer@viwid.com>
2014-10-06 00:32:36 -0600

    Initial Commit

---
+++ VIAddressBookKit/VIAddressBookKit.h
@@ -0,0 +1,19 @@
+//
+//  VIAddressBookKit.h
+//  VIAddressBookKit
+//
+//  Created by Nils Fischer on 06.10.14.
+//  Copyright (c) 2014 viWiD Webdesign & iOS Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VIAddressBookKit.
+FOUNDATION_EXPORT double VIAddressBookKitVersionNumber;
+
+//! Project version string for VIAddressBookKit.
+FOUNDATION_EXPORT const unsigned char VIAddressBookKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VIAddressBookKit/PublicHeader.h>
+
+
